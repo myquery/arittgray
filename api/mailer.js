86b8d97a65aabca0abf583a71a9e44932393f86f
Frederick Eze <stanarua@aol.com>
2020-07-23 11:33:24 +0100

    feat(registrant): form added states to form

--- api/mailer.js
+++ api/mailer.js
@@ -1,38 +1,38 @@
-import nodemailer from 'nodemailer';
-import smtpTransport from "nodemailer-smtp-transport";
+// import nodemailer from 'nodemailer';
+// import smtpTransport from "nodemailer-smtp-transport";
 
-export async function mailer(props){
-    const mailer = nodemailer.createTransport(smtpTransport({
-        service: "gmail",
-        host: "smtp.gmail.com",
-        auth: {
+// export async function mailer(props){
+//     const mailer = nodemailer.createTransport(smtpTransport({
+//         service: "gmail",
+//         host: "smtp.gmail.com",
+//         auth: {
     
-          user: process.env.MAILER.toString(), // gmail created just for testing purposes
-          pass: process.env.SECRET.toString() // for testing purposes
-        }
-      }));
+//           user: process.env.MAILER.toString(), // gmail created just for testing purposes
+//           pass: process.env.SECRET.toString() // for testing purposes
+//         }
+//       }));
 
-      const mailOptions = {
+//       const mailOptions = {
 
-        from: "<arittgray@gmail.com>",
-        to: props.receiver,
-        subject: props.subject,
-        text: props.text,
-        html: ``
-      };
+//         from: "<arittgray@gmail.com>",
+//         to: props.receiver,
+//         subject: props.subject,
+//         text: props.text,
+//         html: ``
+//       };
     
-      // NOTE!!!
-      //  this is for any developer in the future, info is the second parameter of the callback
-      //  after error, i had to remove it since i wasnt using it currently to fix some codacy issue
-      await mailer.sendMail(mailOptions, (error) => {
-        // console.log(mailOptions, info);
-        if (error) {
-          throw error;
-          // console.log(error);
-        }
-        return "Mail sent";
-      });
-     return mailer; 
-    };
+//       // NOTE!!!
+//       //  this is for any developer in the future, info is the second parameter of the callback
+//       //  after error, i had to remove it since i wasnt using it currently to fix some codacy issue
+//       await mailer.sendMail(mailOptions, (error) => {
+//         // console.log(mailOptions, info);
+//         if (error) {
+//           throw error;
+//           // console.log(error);
+//         }
+//         return "Mail sent";
+//       });
+//      return mailer; 
+//     };
     
 
